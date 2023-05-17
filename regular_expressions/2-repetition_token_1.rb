bb973d19147e78b0a5c9d95be29ff7c79d7903ff
angeiradu <a.iradukund2@alustudent.com>
2023-05-17 06:03:50 -0700

    task 2

--- regular_expressions/2-repetition_token_1.rb
+++ regular_expressions/2-repetition_token_1.rb
@@ -4,7 +4,4 @@ input = ARGV[0]
 pattern = /^htb{n}$/
   matches=input.match(pattern)
 if input =~ pattern
-  puts "Input matches the pattern!"
-else
-  puts "Input does not match the pattern."
-end
+  print (matches[0])
