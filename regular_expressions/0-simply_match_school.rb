9714e28971f572915d62440d75a9c79c8e258a1a
angeiradu <a.iradukund2@alustudent.com>
2023-05-17 00:10:22 -0700

    task 0

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,14 +1,11 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env rub
 #import re
+#text_to_search='''
+#School is the best
+#School
+#School is School
+#pattern = re.compiler(r'School\.')
+#matches = pattern.finditer(text_to_search)
 
-text_to_search =
-School is best
-School
-School is School
-
-pattern = re.compiler(r'School\.')
-
-matches = pattern.finditer(text_to_search)
-
-for match in matches:
-    print(match)
+#for match in matches:
+#print(match)
