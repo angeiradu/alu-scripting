28d1516831f4add5504a4c88832c28c15e2e8437
angeiradu <a.iradukund2@alustudent.com>
2023-05-17 04:39:45 -0700

    task 1

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,10 +1,2 @@
 #!/usr/bin/env ruby
-input = ARVG[0]
-
-pattern=/hbt[2,3]n/
-  match = input.match(pattern)
-if match
-  puts "Match found: #{match[0]}"
-else
-  puts "No match found."
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
