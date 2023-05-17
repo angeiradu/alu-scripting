6bc3bb1567aa80acda75a6d8e5ab4207ee6a546d
angeiradu <a.iradukund2@alustudent.com>
2023-05-17 06:20:00 -0700

    task 2

--- regular_expressions/2-repetition_token_1.rb
+++ regular_expressions/2-repetition_token_1.rb
@@ -1,7 +1,8 @@
 #!/usr/bin/env ruby
 input = ARGV[0]
 
-pattern = /^htb{n}$/
-  matches=input.match(pattern)
-if input =~ pattern
-  print (matches[0])
+pattern = /^ht(b?)n$/
+matches=input.match(pattern)
+
+for match in matches
+  print(match)
