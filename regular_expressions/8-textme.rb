73cefddc303782fc44b467e4fa40aff00fb9d7d2
angeiradu <a.iradukund2@alustudent.com>
2023-05-19 02:36:27 -0700

    task 9

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-input = ARGV[0]
-pattern = \[([\w\s+\-\(\)]+)\],\[(\+?\d+[\d\s\(\)\-]+)\],\[(\w+)\]
-matches = input.scan(pattern)
-puts matches[0]
+Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
+List = [Format[0].compact, Format[1].compact, Format[2].compact]
+puts List.join(',')
