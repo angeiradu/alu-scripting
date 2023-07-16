5f9a0db292fe7490673894b445144e2f57543a79
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-07-16 00:00:29 -0700

    task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -10,8 +10,8 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    rq = requests.get('http://www.reddit.com/r/{}
-    /about.json'.format(subreddit),
+    rq = requests.get(
+        'http://www.reddit.com/r/{}/about.json'.format(subreddit),
                      headers={'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /i/ange_movie_md)'}).json()
     subs = rq.get("data", {}).get("subscribers", 0)
